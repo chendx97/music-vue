f7b4fdd2560b6015f9955917e520b786b21c3714
chendx97 <3310115926@qq.com>
2019-11-12 16:25:25 +0800

    public style

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
-  <div class="home">
-    home
-  </div>
+	<div class="home">
+		<div class="tab-wrap">
+			<div>推荐</div>
+			<div>个人中心</div>
+		</div>
+	</div>
 </template>
 
 <script>
 import api from '@/api'
 
 export default {
-  name: 'home',
-  mounted() {
-    // api.getRecommendSongList();
-  }
+	name: 'home',
+	mounted () {
+		// api.getRecommendSongList();
+	}
 }
 </script>
