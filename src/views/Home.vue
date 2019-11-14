43f7483e0b3b6495c6778af4f3cfaf30a98d5543
chendx97 <3310115926@qq.com>
2019-11-14 15:16:13 +0800

    switch tab

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,16 @@
 <template>
 	<div class="home">
 		<div class="header">
-			<div class="setting">set</div>
-			<div class="tab-wrap">
-				<div>推荐</div>
-				<div>个人中心</div>
+			<div class="header-menu">
+				<div class="setting">set</div>
+				<div class="tab-wrap">
+					<div :class="currentTab ? '' : 'active'" @click="switchTab(0)">推荐</div>
+					<div :class="currentTab ? 'active' : ''" @click="switchTab(1)">个人中心</div>
+				</div>
+				<div class="search">search</div>
 			</div>
-			<div class="search">search</div>
 		</div>
+		<div class="body">{{currentTab ? personal : recommand}}</div>
 	</div>
 </template>
 
@@ -16,12 +19,54 @@
 
 export default {
 	name: 'home',
+	data () {
+		return {
+			currentTab: 0,
+			recommand: 'aa',
+			personal: 'bb'
+		}
+	},
 	mounted () {
 		// api.getRecommendSongList(res => {
 		// 	console.log(res)
 		// 	this.result = res.data.result
 		// }, err => {
 		// })
+	},
+	methods: {
+		switchTab (idx) {
+			this.currentTab = idx
+		}
 	}
 }
 </script>
+
+<style lang="stylus" scoped>
+.home {
+	.header {
+		background: #d43c33;
+		color: #fff;
+		height: 40vw;
+		padding: 0 5vw;
+
+		.header-menu {
+			height: 10vw;
+			line-height: 10vw;
+			display: flex;
+			justify-content: space-between;
+
+			.tab-wrap {
+				width: 40%;
+				display: flex;
+				justify-content: space-between;
+				font-size: 4vw;
+				color: #f18686;
+
+				.active {
+					color: #fff;
+				}
+			}
+		}
+	}
+}
+</style>
